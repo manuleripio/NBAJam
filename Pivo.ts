34a25fccd2c962ca182cecd4914f5928c941d475
manuleripio <54365877+manuleripio@users.noreply.github.com>
2020-12-04 10:09:22 -0300

    ✨ Criação e edição do arquivo base main.ts

---
+++ Pivo.ts
@@ -0,0 +1,17 @@
+import Jogador from "./Personagem";
+import {Util} from "./Util";
+export class Pivo extends Jogador {
+    constructor(nome: string, team: string, posicao: string, stamina: number, desgaste: number, 
+        velocidade: number, ataque: number, precisao: number, arremesso: number, pontos: number) {
+            super(nome, team, posicao, stamina, desgaste, velocidade, ataque, precisao, arremesso, pontos)
+            this._stamina =  Util.randomizar(100, 1_000);
+            this._desgaste =  Util.randomizar(100, 1_000);
+            this._velocidade =  Util.randomizar(100, 1_000);
+            this._ataque =  Util.randomizar(100, 1_000);
+            this._precisao =  Util.randomizar(100, 1_000);
+            this._arremesso =  Util.randomizar(100, 1_000);
+            this._pontos = pontos;
+
+    }
+
+}
---
+++ module.ts
@@ -0,0 +1,5 @@
+module changeTeams {
+    class teams {
+        
+    }
+}
